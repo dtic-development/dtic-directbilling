ee2f1b4b2472aff41cf754bdccb35f6870fe91f4
Marc Bernet <mbernet@gmail.com>
2015-11-23 17:49:04 +0100

    changes

--- terminate.php
+++ terminate.php
@@ -2,6 +2,10 @@
 require_once('directbilling.php');
 @session_start();
 
+define('DTIC_API_KEY', '966d1e3b3f10b0f4c515e833fd3ea966');
+
+$billing = new DirectBilling(DTIC_API_KEY);
+
 $subsId = $_GET['subscriptionId'];
 $status = $billing->terminateSubscription(md5($subsId));
 
